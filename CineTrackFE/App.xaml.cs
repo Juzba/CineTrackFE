1733c2623a288f81badb751d91628976163a4e76
Jiří Strnadel <Juzba88@gmail.com>
2025-08-13 19:19:17 +0200

    Prism on startup

--- CineTrackFE/App.xaml.cs
+++ CineTrackFE/App.xaml.cs
@@ -1,14 +1,44 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using CineTrackFE.ViewModels;
 using System.Windows;
 
-namespace CineTrackFE
+namespace CineTrackFE;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : PrismApplication
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
     {
+        containerRegistry.Register<MainWindow>();
+        containerRegistry.Register<MainViewModel>();
     }
+
+
+
+    protected override Window CreateShell()
+    {
+        var mainWindow = Container.Resolve<MainWindow>();
+        mainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        mainWindow.Show();
+        mainWindow.DataContext = Container.Resolve<MainViewModel>();
+
+        return mainWindow;
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+    }
+
+
+    protected override void ConfigureViewModelLocator()
+    {
+        base.ConfigureViewModelLocator();
+    }
+
+
+
 
 }
---
+++ CineTrackFE/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineTrackFE.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public DelegateCommand Btn1Command { get; }
+
+
+        public MainViewModel()
+        {
+            Btn1Command = new DelegateCommand(Btn1);
+        }
+
+
+
+
+        private void Btn1()
+        {
+            // Your logic here
+        }
+
+
+    }
+}
