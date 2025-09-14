1e33d5e82b4888cd9d8597a0107b6752e8c0ae83
Jiří Strnadel <Juzba88@gmail.com>
2025-09-14 20:52:23 +0200

    Zlepšení API pro statistiky a nové datové struktury
    Byly provedeny změny v několika souborech, které se týkají zlepšení API pro statistiky a přidání nových datových struktur pro zpracování statistik.
    
    V `FilmApiControllerTests3.cs` byly upraveny generátory pro komentáře. V `AdminApiController.cs` došlo k úpravě dotazů pro získání statistik, kde se změnily způsoby zahrnutí hodnocení filmů. Nové třídy `StatisticsDto`, `Overview`, `TopMovies` a `UserActivity` byly přidány do `StatisticsDto.cs`, aby se lépe strukturovala data pro statistiky. V `WebStatisticViewModel.cs` byla přidána logika pro načítání statistik a jejich zpracování. Nakonec, v `WebStatisticView.xaml` byly provedeny změny v uživatelském rozhraní pro zobrazení nových statistik, včetně zobrazení nejlepších a nejpopulárnějších filmů a nejaktivnějších uživatelů.

---
+++ CineTrackFE/Models/DTO/StatisticsDto.cs
@@ -0,0 +1,34 @@
+﻿namespace CineTrackFE.Models.DTO;
+
+public class StatisticsDto
+{
+    public Overview Overview { get; set; } = new();
+    public TopMovies TopMovies { get; set; } = new();
+    public UserActivity UserActivity { get; set; } = new();
+
+}
+
+public class Overview
+{
+    public int TotalMovies { get; set; }
+    public int TotalUsers { get; set; }
+    public int TotalRatings { get; set; }
+    public double AverageRating { get; set; }
+    public int TotalComments { get; set; }
+
+}
+
+public class TopMovies
+{
+    public List<Film> BestRated { get; set; } = [];
+    public List<Film> MostPopular { get; set; } = [];
+    public List<Film> Newest { get; set; } = [];
+}
+
+
+public class UserActivity
+{
+    public double AverageCommentsPerUser { get; set; }
+    public List<User> MostActiveUsers { get; set; } = [];
+
+}
--- CineTrackFE/ViewModels/Admins/WebStatisticViewModel.cs
+++ CineTrackFE/ViewModels/Admins/WebStatisticViewModel.cs
@@ -1,6 +1,91 @@
-﻿namespace CineTrackFE.ViewModels.Admins
+﻿using CineTrackFE.AppServises;
+using CineTrackFE.Models;
+using CineTrackFE.Models.DTO;
+using System.Collections.ObjectModel;
+
+namespace CineTrackFE.ViewModels.Admins
 {
-    public class WebStatisticViewModel : BindableBase
+    public class WebStatisticViewModel : BindableBase, INavigationAware
     {
+
+        private readonly IApiService _apiService;
+
+        private readonly AsyncDelegateCommand GetStatisticsAsyncCommand;
+
+
+
+
+        public WebStatisticViewModel(IApiService apiService)
+        {
+            _apiService = apiService;
+            GetStatisticsAsyncCommand = new AsyncDelegateCommand(GetStatisticsAsync);
+            GetStatisticsAsyncCommand.Execute();
+
+        }
+
+
+        // I-NAVIGATION-AWARE //
+        public bool IsNavigationTarget(NavigationContext navigationContext) => false;
+        public void OnNavigatedTo(NavigationContext navigationContext) { }
+        public void OnNavigatedFrom(NavigationContext navigationContext) { }
+
+
+        public async Task GetStatisticsAsync()
+        {
+            try
+            {
+                var response = await _apiService.GetAsync<StatisticsDto>("/api/AdminApi/Statistics");
+                if (response != null)
+                {
+                    Statistics = response;
+                    MostPopularFilms = new ObservableCollection<Film>(response.TopMovies.MostPopular);
+                    TopRatedFilms = new ObservableCollection<Film>(response.TopMovies.BestRated);
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+
+
+        // STATISTICS //
+        private StatisticsDto statistics = new();
+        public StatisticsDto Statistics
+        {
+            get { return statistics; }
+            set { SetProperty(ref statistics, value); }
+        }
+
+        // MOST POPULAR FILMS //
+        private ObservableCollection<Film> mostPopularFilms = [];
+        public ObservableCollection<Film> MostPopularFilms
+        {
+            get { return mostPopularFilms; }
+            set { SetProperty(ref mostPopularFilms, value); }
+        }
+
+        // TOP RATED FILMS //
+        private ObservableCollection<Film> topRatedFilms = [];
+        public ObservableCollection<Film> TopRatedFilms
+        {
+            get { return topRatedFilms; }
+            set { SetProperty(ref topRatedFilms, value); }
+        }
+
+
+
+
+
+
+        // ERROR MESSAGE //
+        private string? errorMessage;
+        public string? ErrorMessage
+        {
+            get { return errorMessage; }
+            set { SetProperty(ref errorMessage, value); }
+        }
+
+
     }
 }
